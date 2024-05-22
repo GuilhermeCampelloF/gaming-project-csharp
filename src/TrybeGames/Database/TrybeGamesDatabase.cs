2a4cb629e67f3226f1266d5c2c315c2cb91f51fb
Guilherme Campello Frizzera <guilhermecampello@hotmail.com>
2024-05-22 15:13:34 -0300

    feat: implementing GetGamesOwnedBy() method

--- src/TrybeGames/Database/TrybeGamesDatabase.cs
+++ src/TrybeGames/Database/TrybeGamesDatabase.cs
@@ -25,8 +25,8 @@ public class TrybeGamesDatabase
     // 6. Crie a funcionalidade de buscar jogos comprados por uma pessoa jogadora
     public List<Game> GetGamesOwnedBy(Player playerEntry)
     {
-        // Implementar
-        throw new NotImplementedException();
+        var getGames = Games.Where(game => playerEntry.GamesOwned.Contains(game.Id));
+        return getGames.ToList();
     }
 
 
