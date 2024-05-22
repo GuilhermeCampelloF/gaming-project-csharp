7a02eb444a431202843cba83af5cb0280f387369
Guilherme Campello Frizzera <guilhermecampello@hotmail.com>
2024-05-22 12:18:26 -0300

    feat: implementing AddPlayer() method

--- src/TrybeGames/Controllers/TrybeGamesController.cs
+++ src/TrybeGames/Controllers/TrybeGamesController.cs
@@ -120,8 +120,18 @@ public class TrybeGamesController
     // 1. Crie a funcionalidde para adicionar uma nova pessoa jogadora ao banco de dados
     public void AddPlayer()
     {
-        // implementar
-        Console.WriteLine("Ainda não é possível realizar essa funcionalidade!");
+        Console.WriteLine("Digite o nome do novo jogador(a):");
+
+        var newPlayerName = Console.ReadLine();
+        var newPlayerId = database.Players.Count();
+        
+        var playerToAdd = new Player {
+            Id = newPlayerId + 1,
+            Name = newPlayerName
+        };
+
+        database.Players.Add(playerToAdd);
+        Console.WriteLine("Novo jogador adicionado!");
     }
 
     // 2. Crie a funcionalidade de adicionar um novo estúdio de jogos ao banco de dados
